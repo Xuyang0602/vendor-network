7df357ab559ae3c0a3cdb493bb143d35101c4a37
Xuyang0602 <wang.xuyang0602@gmail.com>
2023-07-31 15:21:41 -0700

    Fixed typo

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_1" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = var.public_subnets[0]
-    availability_zone =  = var.availability_zones[0]  
+    availability_zone = var.availability_zones[0]  
     map_public_ip_on_launch = true
 
     tags = {
